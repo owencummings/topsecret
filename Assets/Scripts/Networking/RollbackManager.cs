c336461f7aaeb790b4498bb5dc1b3a3d3e34619e
Owen Cummings <owenpatrickcummings@gmail.com>
2023-03-06 11:03:38 -0500

    Some verbose brainstorming

--- Assets/Scripts/Networking/RollbackManager.cs
+++ Assets/Scripts/Networking/RollbackManager.cs
@@ -7,6 +7,7 @@ public class RollbackManager : MonoBehaviour
 {
     public int tick = 0;
     public int ticksPerSecond = 60;
+    public float tickRate = (float) tick / (float) ticksPerSecond;
     public List<IList> rollbackableBuffers;
     public bool newServerResponse;
 
@@ -37,6 +38,23 @@ public class RollbackManager : MonoBehaviour
             // - Obvious problem is that includes every grid square...
             // - But we can have a separate interface for INPUTBUFFER which rollbackable will inherit from
             // create  new physics scene
+
+
+            // Objects should be able to subscribe to the rollback calculations
+            // And then, those objects should be able to 'contribute' as they deem necessary
+            // Player objects, for instance, are rollbackable, in that then need state and inputs
+            //      preserved AND need to update their current state after recalculation
+            // Some objects need state and input provided, but don't need to necessarily be rollbackable
+            // because they are 'static' (pylons from the maze, though they could be rollbackable)
+            // And some objects don't have true input at all? And we wont be tracking in the replay step.
+            // What if an object it destroyed client-side when it needs to be rollbacked?
+            // Guess we wouldnt do that... or would set come kind of kill-coroutine
+            // So yeah we want... INPUTBUFFER, STATEBUFFER, and maybe even a result buffer?
+            // All of those 
+            // Something to pass on to server saying we were successful on our end... we can handle that later.
+            // 
+            // How do we subscribe to the rollbackManager? I guess we should use an abstract class
+            // With a built-in subscribe function when its initialized
         }
         // So what would this look like exactly? We would have distributed buffers on each component attached to this one???
         // And then
